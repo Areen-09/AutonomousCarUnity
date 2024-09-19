155091d4e39c43a9dfa43caeb7109a8c719df633
Areen Agrawal <areenagrawal09@gmail.com>
2024-09-19 16:22:00 +0530

    Unity Setup

---
+++ Assets/FollowCar.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class FollowCar : MonoBehaviour
+{
+    public Transform car;
+    public Vector3 offset;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = car.position + offset;
+    }
+}
